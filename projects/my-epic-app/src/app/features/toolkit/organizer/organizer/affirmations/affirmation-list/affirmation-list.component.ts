a895536eef6688f8a11627e222d398edb5819476
Patrycja Swierkowska <patricija.swierkowska@gmail.com>
2021-03-11 16:35:54 +0100

    feat(auth) implement auth changes and take oservables

--- projects/my-epic-app/src/app/features/toolkit/organizer/organizer/affirmations/affirmation-list/affirmation-list.component.ts
+++ projects/my-epic-app/src/app/features/toolkit/organizer/organizer/affirmations/affirmation-list/affirmation-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Affirmation } from '../affirmation.model';
+import { AffirmationService } from '../affirmation.service';
 
 // import {MatPaginator} from '@angular/material/paginator';
 // import {MatSort} from '@angular/material/sort';
@@ -13,27 +15,30 @@ import { Affirmation } from '../affirmation.model';
 export class AffirmationListComponent implements OnInit {
   @Output() affirmationWasSelected = new EventEmitter<Affirmation>();
 
-  affirmations: Affirmation[] = [
-    new Affirmation(
-      'A Test affirmation',
-      'This is simply a test',
-      'lorem ipsum dolor',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-    ),
-    new Affirmation(
-      'Another Test affirmation',
-      'This is simply a test',
-      'loremmmmm ipsummmm memememe ',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-    ),
-    new Affirmation(
-      'third',
-      'This is weird',
-      'wtf',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-    )
-  ];
-  constructor() {}
+  affirmations$: Observable<any[]> = this.affService.kotek;
+  // affirmations: Affirmation[] = [
+  //   new Affirmation(
+  //     'A Test affirmation',
+  //     'This is simply a test',
+  //     'lorem ipsum dolor',
+  //     'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+  //   ),
+  //   new Affirmation(
+  //     'Another Test affirmation',
+  //     'This is simply a test',
+  //     'loremmmmm ipsummmm memememe ',
+  //     'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+  //   ),
+  //   new Affirmation(
+  //     'third',
+  //     'This is weird',
+  //     'wtf',
+  //     'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+  //   )
+  // ];
+  constructor(private affService: AffirmationService) {
+    // this.affirmations$.subscribe(console.log);
+  }
 
   ngOnInit(): void {}
   onAffirmationSelected(affirmation: Affirmation) {
---
+++ projects/my-epic-app/src/app/features/toolkit/organizer/organizer/affirmations/affirmation.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../../../../../shared/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AffirmationService {
+  constructor(private firestore: AngularFirestore, private auth: AuthService) {}
+  user = this.auth.userData;
+  kotek: Observable<any[]> = this.auth.user$;
+}
